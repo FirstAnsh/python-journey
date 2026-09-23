72ee56859ea2e87719306aec7c1d7815d1c32151
Ansh <ansh117.ry@gmail.com>
2026-09-23 14:45:32 +0530

    Add DSA even odd counter

---
+++ dsa/03_count_even_odd.py
@@ -0,0 +1,14 @@
+numbers = [12, 45, 7, 89, 34, 56]
+
+even_count = 0
+odd_count = 0
+
+for number in numbers:
+    if number % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print(f"Numbers: {numbers}")
+print(f"Even numbers: {even_count}")
+print(f"Odd numbers: {odd_count}")
