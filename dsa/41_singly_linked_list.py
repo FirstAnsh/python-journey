3e56f230fd360379746a5de1013bce366c68d040
Ansh <ansh117.ry@gmail.com>
2026-09-26 20:13:06 +0530

    Add problem 41: singly linked list implementaion

---
+++ dsa/41_singly_linked_list.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        print(" -> ".join(elements) if elements else "List is empty")
+
+
+# Interactive execution
+ll = LinkedList()
+raw_input = input("Enter values to insert into linked list (space-separated): ")
+for item in raw_input.split():
+    ll.append(item)
+
+print("Linked List representation:")
+ll.display()
